ecce2848353425bc235c2c2999a7d1e6ffb17de3
Tolmeros@github.com <test>
2022-04-13 14:51:56 +0300

    Changed player marks

--- src/components/TicTacToe/Cell/index.js
+++ src/components/TicTacToe/Cell/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import imgX from "../img_X.png"
+import imgO from "../img_O.png"
+
+const playerMark = (player) => {
+  if (player === 'x') {
+    return <img src={imgX} alt="X" />;
+  }
+  if (player === 'o') {
+    return <img src={imgO} alt="O" />;
+  }
+  return null;
+};
 
 const Cell = ({num, value, handleClick}) => {
-  return <td onClick={() => handleClick(num)}>{value}</td>;
+  return <td onClick={() => handleClick(num)}>{playerMark(value)}</td>;
 };
 
 export default Cell;
